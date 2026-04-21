48d83299d2a12f2bc602ba34dd43692cb32fb5e8
Gustavo <gustavo.loper@blueshift.cc>
2026-04-21 01:17:36 -0300

    Fix i3 status bar

--- home/gustavo.nix
+++ home/gustavo.nix
@@ -8,10 +8,69 @@
 
   programs.home-manager.enable = true;
 
+  ########################################
+  # 📦 PACOTES
+  ########################################
   home.packages = with pkgs; [
     git
     neovim
     brave
     rio
+    rofi
+    feh
   ];
+
+  ########################################
+  # 🪟 I3
+  ########################################
+  xsession.enable = true;
+
+  xsession.windowManager.i3 = {
+    enable = true;
+
+    config = {
+      modifier = "Mod4";
+      terminal = "rio";
+
+      keybindings = {
+        "Mod4+d" = "exec rofi -show drun";
+      };
+
+      bars = [
+        {
+          statusCommand = "i3status-rs";
+        }
+      ];
+    };
+  };
+
+  ########################################
+  # 📊 I3STATUS-RUST (AQUI ESTÁ A MÁGICA)
+  ########################################
+  programs.i3status-rust = {
+    enable = true;
+
+    bars = {
+      default = {
+        settings = {
+          theme = {
+            theme = "dracula";
+          };
+
+          icons = "awesome6";
+
+          blocks = [
+            { block = "cpu"; }
+            { block = "memory"; }
+            { block = "disk_space"; path = "/"; }
+            {
+              block = "time";
+              interval = 60;
+              format = "%H:%M";
+            }
+          ];
+        };
+      };
+    };
+  };
 }
