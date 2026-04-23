627716d6cfc32b81e2c21a9373d7fc991e2757e5
Gustavo <gustavo.loper@blueshift.cc>
2026-04-23 16:48:26 -0300

    Fix i3

--- home/gustavo.nix
+++ home/gustavo.nix
@@ -7,58 +7,118 @@
 
   programs.home-manager.enable = true;
 
-  ########################################
-  # PACOTES DO USUÁRIO
-  ########################################
   home.packages = with pkgs; [
     git
     neovim
     brave
     rio
     feh
-    ulauncher
     rofi
+    ulauncher
+    i3status-rust
+    xorg.xrandr
+    xorg.setxkbmap
   ];
 
-  ########################################
-  # XSESSION / I3
-  ########################################
+  programs.i3status-rust = {
+    enable = true;
+
+    bars = {
+      default = {
+        settings = {
+          theme = {
+            theme = "dracula";
+          };
+
+          icons = "awesome6";
+
+          blocks = [
+            { block = "cpu"; }
+            { block = "memory"; }
+            { block = "disk_space"; path = "/"; }
+            {
+              block = "time";
+              interval = 60;
+              format = "%d/%m %H:%M";
+            }
+          ];
+        };
+      };
+    };
+  };
+
   xsession.enable = true;
 
   xsession.windowManager.i3 = {
     enable = true;
 
-    config = {
-      modifier = "Mod4";
+    config = let
+      mod = "Mod4";
+    in {
+      modifier = mod;
       terminal = "rio";
 
-      ########################################
-      # ATALHOS
-      ########################################
-      keybindings = let
-        mod = "Mod4";
-      in {
-        # terminal
-        "${mod}+Return" = "exec rio";
+      startup = [
+        {
+          command = "feh --bg-fill /home/gustavo/nix-config/assets/wallpapers/wallpaper1.jpg";
+          always = true;
+        }
 
-        # launcher
+        {
+          command = "ulauncher --hide-window";
+          always = false;
+        }
+
+        {
+          command = "xrandr --output HDMI-0 --mode 1920x1080 --rotate left --pos 0x0";
+          always = true;
+        }
+
+        {
+          command = "xrandr --output DP-0 --primary --mode 3440x1440 --rotate normal --pos 1080x0";
+          always = true;
+        }
+
+        {
+          command = "xrandr --output DP-0 --primary";
+          always = true;
+        }
+
+        {
+          command = "i3-msg 'workspace 1'";
+          always = true;
+        }
+
+        {
+          command = "i3-msg 'workspace 2'";
+          always = true;
+        }
+
+      ];
+
+      workspaceOutputAssign = [
+        {
+          workspace = "1";
+          output = "DP-0";
+        }
+        {
+          workspace = "2";
+          output = "HDMI-0";
+        }
+      ];
+
+      keybindings = {
+        "${mod}+Return" = "exec rio";
         "${mod}+d" = "exec ulauncher-toggle";
 
-        # fechar janela
         "${mod}+Shift+q" = "kill";
-
-        # reload
         "${mod}+Shift+r" = "reload";
-
-        # restart
         "${mod}+Shift+c" = "restart";
 
-        # fullscreen
         "${mod}+f" = "fullscreen toggle";
 
-        # alternar idioma
-        "${mod}+space" =
-          "exec setxkbmap -layout br,us -option grp:alt_shift_toggle";
+        "${mod}+space" = "exec setxkbmap -layout us";
+        "${mod}+Shift+space" = "exec setxkbmap -layout br";
 
         # workspaces
         "${mod}+1" = "workspace 1";
@@ -72,7 +132,7 @@
         "${mod}+9" = "workspace 9";
         "${mod}+0" = "workspace 10";
 
-        # mover janela
+        # mover janelas
         "${mod}+Shift+1" = "move container to workspace 1";
         "${mod}+Shift+2" = "move container to workspace 2";
         "${mod}+Shift+3" = "move container to workspace 3";
@@ -84,41 +144,18 @@
         "${mod}+Shift+9" = "move container to workspace 9";
         "${mod}+Shift+0" = "move container to workspace 10";
 
-        # navegação estilo vim
+        # navegação
         "${mod}+h" = "focus left";
         "${mod}+j" = "focus down";
         "${mod}+k" = "focus up";
         "${mod}+l" = "focus right";
       };
 
-      ########################################
-      # STARTUP
-      ########################################
-      startup = [
-        {
-          command = "feh --bg-fill /home/gustavo/nix-config/assets/wallpapers/wallpaper1.jpg";
-          always = true;
-        }
-        {
-          command = "ulauncher --hide-window";
-          always = false;
-        }
-      ];
-
-      ########################################
-      # BARRA
-      ########################################
       bars = [
         {
-          statusCommand = "i3status";
+          statusCommand = "i3status-rs ~/.config/i3status-rust/config.toml";
         }
       ];
     };
   };
-
-  ########################################
-  # ARQUIVOS DO USUÁRIO
-  ########################################
-  home.file = { };
-
 }
