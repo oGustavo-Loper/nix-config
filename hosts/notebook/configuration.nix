7f5be8149a7217becc0cbbcfa3d2b8b40342823a
Gustavo <gustavo.loper@blueshift.cc>
2026-04-23 20:30:19 -0300

    Fix notebook

--- hosts/notebook/configuration.nix
+++ hosts/notebook/configuration.nix
@@ -18,8 +18,8 @@
 
   services.xserver.enable = true;
 
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   services.xserver.windowManager.i3.enable = true;
 
